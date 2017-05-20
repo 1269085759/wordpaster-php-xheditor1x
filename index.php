f73f76c65063dc663a4b88cf8c8bf5a7030395bb
xfl <xfl@ncmem.com>
2017-05-20 17:15:38 +0800

    增加js引用

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     <link type="text/css" rel="Stylesheet" href="WordPaster/css/WordPaster.css"/>
     <link type="text/css" rel="Stylesheet" href="WordPaster/js/skygqbox.css" />
     <script type="text/javascript" src="WordPaster/js/skygqbox.js" charset="utf-8"></script>
+	<script type="text/javascript" src="WordPaster/js/w.edge.js" charset="utf-8"></script>
+    <script type="text/javascript" src="WordPaster/js/w.app.js" charset="utf-8"></script>
+    <script type="text/javascript" src="WordPaster/js/w.file.js" charset="utf-8"></script>
 	<script type="text/javascript" src="WordPaster/js/WordPaster.js" charset="utf-8"></script>
 	<style type="text/css">
 		.imagepaster{background: transparent url(WordPaster/paster.png) no-repeat; background-position:3px 2px;}
